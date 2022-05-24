9cd0649366195105d134bd35a6adc49670aa140e
mindtreejanedge14 <mindtreejanedge14@mindfeb763.iiht.tech>
2022-05-24 09:56:31 +0530

    initial commit

---
+++ EmployeeManagment/src/main/java/EmoloyeeManagment/employeemanagment.java
@@ -0,0 +1,11 @@
+package EmoloyeeManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class employeemanagment {
+public static void main(String[] args) {
+	SpringApplication.run(employeemanagment.class, args);
+}
+}
